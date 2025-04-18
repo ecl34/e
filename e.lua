8a42e2fe9414440b0a06fe94daadf276e2b88f3c
ecl34 <kentalworkz@gmail.com>
2025-04-18 20:48:14 +0800

    Update e.lua

--- e.lua
+++ e.lua
@@ -1,4 +1,4 @@
-local whitelist = {
+return {
 	["sdjaYsndshsXZNSHasH"] = {
 		"534993e8-a6b7-467b-acba-5955d11bc481",
 		"6C4F8286-086A-4710-86E8-A1B5E8435552",
@@ -10,7 +10,7 @@ local whitelist = {
 		hwids = {
 			"F3BA7C06-CBE0-40EF-9222-1B0D731FE409",
 		},
-		expiration = 1747752830, -- 20 Mei 2025 19:53:50 WITA (UNIX time)
+		expiration = 1747752830,
 	},
 	["MNHs729dAsXjKaOq2"] = "ALL"
 }
