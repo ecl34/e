84ed3e8d5b27203b2838416391508772bbc3a9ee
ecl34 <kentalworkz@gmail.com>
2025-04-25 20:37:48 +0800

    Update e.lua

--- e.lua
+++ e.lua
@@ -1,16 +1,16 @@
-local whitelist = {
-	["sdjaYsndshsXZNSHasH"] = {
-		"534993e8-a6b7-467b-acba-5955d11bc481",
-		"6C4F8286-086A-4710-86E8-A1B5E8435552",
-	},
-	["HsnscatwdSASnshA"] = {
-		"73199275-9C31-4644-B92B-45F360255FBE",
-	},
-	["NzpLxvAqMdTeRwHb"] = {
-		hwids = {
-			"F3BA7C06-CBE0-40EF-9222-1B0D731FE409",
-		},
-		expiration = 1748180030,
-	},
-	["MNHs729dAsXjKaOq2"] = "ALL"
+return {
+    ["sdjaYsndshsXZNSHasH"] = {
+        "534993e8-a6b7-467b-acba-5955d11bc481",
+        "6C4F8286-086A-4710-86E8-A1B5E8435552"
+    },
+    ["HsnscatwdSASnshA"] = {
+        "73199275-9C31-4644-B92B-45F360255FBE"
+    },
+    ["NzpLxvAqMdTeRwHb"] = {
+        hwids = {
+            "F3BA7C06-CBE0-40EF-9222-1B0D731FE409"
+        },
+        expiration = 1748180030
+    },
+    ["MNHs729dAsXjKaOq2"] = "ALL"
 }
