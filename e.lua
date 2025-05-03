afe8f90c537b8bab3d73e93b791c2d434ccc2e6b
ecl34 <kentalworkz@gmail.com>
2025-05-03 22:52:47 +0800

    Update e.lua

--- e.lua
+++ e.lua
@@ -4,7 +4,7 @@ return {
         "6C4F8286-086A-4710-86E8-A1B5E8435552"
     },
     ["HsnscatwdSASnshA"] = {
-        "73199275-9C31-4644-B92B-45F360255FBE"
+        "6C060858-94F7-40A6-AAEC-4516CDD14649"
     },
     ["NzpLxvAqMdTeRwHb"] = {
         hwids = {
